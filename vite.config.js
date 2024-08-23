f2c06996fb3ac2a5d94e1347dd1aa7c7da79a131
Goy <lingerer_dev@163.com>
2024-08-23 10:00:35 +0800

    init commit

---
+++ vite.config.js
@@ -0,0 +1,73 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import IconResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import externalGlobals from "rollup-plugin-external-globals";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+
+  build: {
+    target: "esnext",
+
+    rollupOptions: {
+      input: {
+        home: path.resolve(__dirname, "index.html"),
+      },
+      external: ["vue", "mapbox-gl"],
+      plugins: [
+        externalGlobals({
+          vue: "Vue",
+          "mapbox-gl": "mapboxgl",
+        }),
+      ],
+    },
+  },
+
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [IconResolver()],
+    }),
+    Icons({}),
+    createHtmlPlugin({
+      template: "./index.html",
+      inject: {
+        tags: [
+          {
+            injectTo: "head",
+            tag: "script",
+            attrs: {
+              src: "https://unpkg.com/vue@3.4.37/dist/vue.global.prod.js",
+            },
+          },
+          {
+            injectTo: "head",
+            tag: "script",
+            attrs: {
+              src: "https://unpkg.com/mapbox-gl@3.6.0/dist/mapbox-gl.js",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/style/mixin.scss";',
+      },
+    },
+  },
+});
