f5cba90e9d74c8bb7573f71047ab47aa53e40619
goy <lingerer_dev@163.com>
2025-01-13 10:45:23 +0800

    build(commitlint): config commitlint work with cz
    配置commitlint prompt，使用cz

--- apps/map-mapbox/src/main.js
+++ apps/map-mapbox/src/main.js
@@ -1,30 +1,30 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-import router from '@/router'
-import App from './App.vue'
-import { nsLogger } from '@lingr/map-core'
-import 'vuetify/styles'
-import './style.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
+import router from '@/router';
+import App from './App.vue';
+import { nsLogger } from '@lingr/map-core';
+import 'vuetify/styles';
+import './style.css';
 
-nsLogger.init(['*:CFG', '*:DBG', '*:LOG', '*:INF', '*:WRN', '*:ERR', '*:NOTE'])
+nsLogger.init(['*:CFG', '*:DBG', '*:LOG', '*:INF', '*:WRN', '*:ERR', '*:NOTE']);
 
-const { appVersion } = navigator
+const { appVersion } = navigator;
 if (appVersion.indexOf('Win') >= 0) {
-    document.body.classList.add('os-windows')
+  document.body.classList.add('os-windows');
 } else if (appVersion.indexOf('Mac') >= 0) {
-    document.body.classList.add('os-mac')
+  document.body.classList.add('os-mac');
 } else if (appVersion.indexOf('Linux') >= 0) {
-    document.body.classList.add('os-linux')
+  document.body.classList.add('os-linux');
 }
 
-const vuetify = createVuetify({})
+const vuetify = createVuetify({});
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(vuetify)
-app.use(pinia)
-app.use(router)
+app.use(vuetify);
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
